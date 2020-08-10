b0cd7988aada71c3f21af631b57f1891b09d1619
MidnightSkiesTech <56942538+MidnightSkiesTech@users.noreply.github.com>
2020-08-10 13:18:05 -0400

    Create fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+
+  // Brain explodes here:
+  int i;
+
+  for (i = 1; i <= 100; i++) {
+
+      
+    if (i % 15 == 0) {
+
+      std::cout << "FizzBuzz\n";
+
+    }
+
+   else if (i % 5 == 0) {
+
+     std::cout << "Buzz\n";
+
+    }
+
+    else if (i % 3 == 0) {
+
+      std::cout << "Fizz\n";
+
+    }
+    else {
+
+     std::cout << i << "\n";
+
+    }
+
+    
+  }
+return 0;
+
+}
