8d59b6bde13ddcb2b7a1f2ba9db9f6ebfc09d9a4
MidnightSkiesTech <56942538+MidnightSkiesTech@users.noreply.github.com>
2020-08-10 14:06:51 -0400

    Create whale.cpp

---
+++ whale.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+  std::string input = "The quick brown fox jumped over the lazy dog.";
+
+  std::vector<char> vowels = {'a','e','i','o','u'};
+  std::vector<char> result;
+
+ for (int i = 0; i < input.size(); i++) {
+
+   for (int j = 0; j < vowels.size(); j++) {
+
+     if (input[i] == vowels[j]) {
+
+        result.push_back(input[i]);     
+
+     }
+
+   }
+
+  if (input[i] == 'u' || input[i] == 'e') {
+
+    result.push_back(input[i]);
+  }
+
+  }
+  for (int k = 0; k < result.size(); k++) {
+
+  std::cout << result[k];
+
+
+}
+
+  std::cout << "\n";
+  return 0;
+  
+
+}
